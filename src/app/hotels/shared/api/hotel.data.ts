282588c2fe78f4f3379caa09f098a793f9ca89c5
jamiben <jamilazerara@yahoo.fr>
2022-10-26 17:31:37 +0200

    correction hotel.data.ts

--- src/app/hotels/shared/api/hotel.data.ts
+++ src/app/hotels/shared/api/hotel.data.ts
@@ -1,37 +1,39 @@
+import { Component } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { IHotel } from '../models/hotel';
 
-export class HotelData Implements InMemoryDbService {
 
-  createDb(): Record<string IHotel[]> {
+export class HotelData implements InMemoryDbService {
+
+  createDb(): Record<string, IHotel[]> {
 
     const hotels: IHotel[] = [
       {
         id: 1,
         hotelName:"Thany",
-        hotelDescription:"lorem ipsum",
-        hotelImageUrl:"assets/img/thany.jpg",
-        hotelPrice:200.60,
+        description:"lorem ipsum",
+        imageUrl:"assets/img/thany.jpg",
+        price:200.60,
         rating:3.5
 
       },
       {
         id:2,
         hotelName:"Marahba",
-        hotelDescription:"lorem ipsum",
-        hotelImageUrl:"assets/img/marhaba.jpg",
-        hotelPrice:300.2,
+        description:"lorem ipsum",
+        imageUrl:"assets/img/marhaba.jpg",
+        price:300.2,
         rating:5
       },
       {
         id:3,
         hotelName:"Lesehan",
-        hotelDescription:"lorem ipsum",
-        hotelImageUrl:"assets/img/lesehan-cahayah.png",
-        hotelPrice:400.5,
+        description:"lorem ipsum",
+        imageUrl:"assets/img/lesehan-cahayah.png",
+        price:400.5,
         rating: 3.5
       }
-    ],
+    ]
 
     return { hotels };
   }
