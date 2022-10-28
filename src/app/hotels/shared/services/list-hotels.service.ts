885b6cdd0581478b10dee2a7db492072d6f781e5
jamiben <jamilazerara@yahoo.fr>
2022-10-28 14:40:57 +0200

    services genId

--- src/app/hotels/shared/services/list-hotels.service.ts
+++ src/app/hotels/shared/services/list-hotels.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { getNgModuleById, Injectable } from "@angular/core";
 import { Observable, throwError, of } from "rxjs";
 import { catchError, map, tap } from "rxjs/operators";
 import { IHotel } from "../models/hotel";
+import { HotelData } from "../api/hotel.data";
 
 @Injectable({
     providedIn: 'root'
@@ -35,6 +36,11 @@ export class HotelListService {
   }
 
   public createHotel(hotel: IHotel): Observable<IHotel> {
+    hotel = {
+      ...hotel,
+      imageUrl: 'assets/img/thany.jpg'
+    };
+console.log('hotel createHotel: ', hotel)
     return this.http.post<IHotel>(this.HOTEL_API_URL, hotel).pipe(
       catchError(this.handleError)
     );
@@ -42,35 +48,54 @@ export class HotelListService {
 
   public updatehotel(hotel: IHotel): Observable<IHotel> {
 
-    const url = `${this.HOTEL_API_URL}/${hotel.id}` ;
+    const url = `${this.HOTEL_API_URL}/${hotel.id}222` ;
     return this.http.put<IHotel>(url, hotel).pipe(
       catchError(this.handleError)
     );
   }
 
+public deleteHotel(id: number): Observable<{}> {
+  const url = `${this.HOTEL_API_URL}/${id}`;
+
+  return this.http.delete<IHotel>(url).pipe(
+    catchError(this.handleError)
+  );
+}
+
   private getDefaultHotel(): IHotel {
-    return {
+    const newHotel = {
       id: 0,
-      hotelName: null,
-      description: null,
-      price: null,
-      rating: null,
-      hotelImageUrl: null
+      hotelName: '',
+      description: '',
+      price: 0,
+      rating: 0,
+      imageUrl: '',
     };
+
+    return newHotel;
   }
 
   private handleError(error: HttpErrorResponse) {
+    let errorMessage: string;
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
+      console.error('An error occurred:', error.error.message);
+      errorMessage=`An error occured: ${error.error.message}`;
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, `+
+        `body was: ${error.error}`),
+      errorMessage =`Backend returned code ${error.status},`+
+        `body was: ${error.error}`;
+
     }
     // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(() => new Error(
+      'Something bad happened; please try again later.' +
+      '\n' +errorMessage
+    ));
   }
 
 
